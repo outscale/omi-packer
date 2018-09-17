1356098db785394558934a1b3ad7cfce6a48802b
Romain Lecat <romain.lecat@outscale.com>
2018-09-17 15:32:27 +0200

    Revert "Test Ubuntu as builder"
    This reverts commit 757b7c5b7b030775bda789441ec1006f0d8fc37f.

--- scripts/chroot_centos7.sh
+++ scripts/chroot_centos7.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #### BASIC IMAGE
-#sudo yum install -y wget
-sudo apt install -y wget
+sudo yum install -y wget
 cd /tmp
 wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.raw.tar.gz
 tar -zxvf CentOS-7-x86_64-GenericCloud.raw.tar.gz && mv *.raw centos7.raw
