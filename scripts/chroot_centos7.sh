f16e1f28f688aff77a1cb09cab2dab05208f62da
Romain Lecat <romain.lecat@outscale.com>
2018-09-17 17:17:44 +0200

    Test if that fixes the issue

--- scripts/chroot_centos7.sh
+++ scripts/chroot_centos7.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #### BASIC IMAGE
+sudo yum install -y wget
 cd /tmp
 wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.raw.tar.gz
 tar -zxvf CentOS-7-x86_64-GenericCloud.raw.tar.gz && mv *.raw centos7.raw
@@ -10,7 +11,8 @@ sudo mount /dev/sda1 /mnt
 
 #### CHROOT FIXES
 sudo cp /etc/resolv.conf /mnt/etc/resolv.conf
-sudo mount -o bind /dev /mnt/dev
+sudo mount -o bind /dev/random /mnt/dev/random
+sudo mount -o bind /dev/urandom /mnt/dev/urandom
 sudo mount -o bind /proc /mnt/proc
 sudo mount -o bind /sys /mnt/sys
 
@@ -19,6 +21,9 @@ sudo chroot /mnt/ yum upgrade -y
 sudo chroot /mnt/ yum clean all
 
 #### OUTSCALE PACKAGES
+sudo chroot /mnt rpm -i http://osu.eu-west-2.outscale.com/outscale-official-packages/udev/osc-udev-rules-20160516-1.x86_64.rpm 
+sudo chroot /mnt rpm -i http://osu.eu-west-2.outscale.com/outscale-official-packages/dhclient-configuration/dhclient-configuration-1.0.0-1-Centos7.x86_64.rpm
+sudo chroot /mnt rpm -i http://osu.eu-west-2.outscale.com/outscale-official-packages/fni/osc-fni-centos-1.0.0-0.x86_64.rpm
 
 #### CLEANUP
 sudo umount /mnt/dev
