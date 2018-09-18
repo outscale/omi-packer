8cf7d52be8a5722514eeeb854bf27eefc92aeea0
Romain Lecat <romain.lecat@outscale.com>
2018-09-18 11:37:57 +0200

    Display progress of dd in Jenkins

--- scripts/chroot_centos7.sh
+++ scripts/chroot_centos7.sh
@@ -5,7 +5,7 @@ yum install -y wget
 cd /tmp
 wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.raw.tar.gz
 tar -zxvf CentOS-7-x86_64-GenericCloud.raw.tar.gz && mv *.raw centos7.raw
-dd if=./centos7.raw of=/dev/sda bs=1G status=progress conv=sparse
+dd if=./centos7.raw of=/dev/sda bs=1G status=progress conv=sparse 2>&1
 mount -o nouuid /dev/sda1 /mnt
 
 #### CHROOT FIXES
