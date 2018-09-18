2dd1e9fb5b6d4b9138bc15fd2ecb4eba92af97c2
Romain Lecat <romain.lecat@outscale.com>
2018-09-18 13:12:41 +0200

    Test for broken permissions

--- scripts/chroot_centos7.sh
+++ scripts/chroot_centos7.sh
@@ -6,7 +6,7 @@ cd /tmp
 wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.raw.tar.gz
 tar -zxvf CentOS-7-x86_64-GenericCloud.raw.tar.gz && mv *.raw centos7.raw
 dd if=./centos7.raw of=/dev/sda bs=1G status=progress conv=sparse
-mount -o nouuid /dev/sda1 /mnt
+mount /dev/sda1 /mnt
 
 #### CHROOT FIXES
 cp /etc/resolv.conf /mnt/etc/resolv.conf
