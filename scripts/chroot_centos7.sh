be5996f326b33647d91185101d65afc46238d4c7
Romain Lecat <romain.lecat@outscale.com>
2018-09-18 10:00:22 +0200

    Better UUID management

--- scripts/chroot_centos7.sh
+++ scripts/chroot_centos7.sh
@@ -6,8 +6,7 @@ cd /tmp
 wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.raw.tar.gz
 tar -zxvf CentOS-7-x86_64-GenericCloud.raw.tar.gz && mv *.raw centos7.raw
 sudo dd if=/tmp/centos7.raw of=/dev/sda bs=1M status=progress
-sudo xfs_admin -U generate /dev/sda1
-sudo mount /dev/sda1 /mnt
+sudo mount -o nouuid /dev/sda1 /mnt
 
 #### CHROOT FIXES
 sudo cp /etc/resolv.conf /mnt/etc/resolv.conf
