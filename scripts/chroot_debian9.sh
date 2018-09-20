054e2e1844d93701e2e81e84042b537eff5bf670
Romain Lecat <romain.lecat@outscale.com>
2018-09-20 09:17:50 +0200

    Initial test for Debian 9

---
+++ scripts/chroot_debian9.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#### BASIC IMAGE
+yum install -y wget
+cd /tmp
+wget https://cdimage.debian.org/cdimage/openstack/current-9/debian-9-openstack-amd64.raw
+mv *.raw debian9.raw
+dd if=./debian9.raw of=/dev/sda bs=1G status=progress conv=sparse
+mount -o nouuid /dev/xvdf /mnt
+
+#### CHROOT FIXES
+cp /etc/resolv.conf /mnt/etc/resolv.conf
+mount -o bind /dev /mnt/dev
+mount -o bind /proc /mnt/proc
+mount -o bind /sys /mnt/sys
+
+#### UPDATES
+chroot /mnt/ apt update -y
+chroot /mnt/ apt upgrade -y
+chroot /mnt/ apt clean
+
+#### OUTSCALE PACKAGES
+wget https://osu.eu-west-2.outscale.com/outscale-official-packages/udev/osc-udev-rules_20160516_amd64.deb -P /mnt/tmp
+chroot /mnt/ dpkg -i /mnt/tmp/*.deb
+
+#### CONFIGURATION
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /mnt/etc/selinux/config
+
+#### CLEANUP
+umount /mnt/dev
+umount /mnt/proc
+umount /mnt/sys
+umount /mnt
+rm -rf /mnt/var/cache/yum
+rm -rf /mnt/root/.ssh
+rm -rf /mnt/root/.bash_history
+rm -rf /mnt/home/centos/.ssh
+rm -rf /mnt/home/centos/.bash_history
+rm -rf /mnt/tmp/*
+rm -rf /mnt/var/lib/dhcp/
+rm -rf /mnt/var/tmp/*
+rm -rf /mnt/var/log/*
+rm -rf /mnt/var/lib/cloud/*
