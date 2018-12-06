2750cc34b7816dce54fa4201eda9bd5a8d4b69e9
Romain Lecat <romain.lecat@outscale.com>
2018-12-06 12:01:06 +0100

    Package list for Ubuntu 18

--- scripts/chroot_ubuntu1804.sh
+++ scripts/chroot_ubuntu1804.sh
@@ -35,6 +35,7 @@ wget https://osu.eu-west-2.outscale.com/outscale-official-packages/fni/osc-fni-1
 chroot /mnt/ dpkg -i /tmp/osc-fni-1.0.0-x86_64.deb
 yes | cp -i /tmp/cloud.cfg /mnt/etc/cloud/cloud.cfg
 yes | cp -i /tmp/sshd_config /mnt/etc/ssh/sshd_config
+chroot /mnt/ apt list --installed > /tmp/packages
 
 #### CLEANUP
 rm -f /mnt/etc/resolv.conf
