0dd0a782101bffbd375469f2a13a8e934c8b0daa
Romain Lecat <romain.lecat@outscale.com>
2019-05-16 15:33:42 +0200

    Try without divert

--- scripts/chroot_ubuntu1804.sh
+++ scripts/chroot_ubuntu1804.sh
@@ -39,8 +39,8 @@ yes | cp -i /tmp/cloud.cfg /mnt/etc/cloud/cloud.cfg
 yes | cp -i /tmp/sshd_config /mnt/etc/ssh/sshd_config
 chroot /mnt/ apt list --installed > /tmp/packages
 
-chroot /mnt/ dpkg-divert --local --divert /etc/cloud/cloud.cfg.default --rename /etc/cloud/cloud.cfg
-chroot /mnt/ dpkg-divert --local --divert /etc/ssh/sshd_config --rename /etc/ssh/sshd_config.default
+#chroot /mnt/ dpkg-divert --local --divert /etc/cloud/cloud.cfg.default --rename /etc/cloud/cloud.cfg
+#chroot /mnt/ dpkg-divert --local --divert /etc/ssh/sshd_config --rename /etc/ssh/sshd_config.default
 
 #### CLEANUP
 rm -f /mnt/etc/resolv.conf
