3677d914ebe7726de1048f3fffdaf74ca5719d6d
romain.lecat <romain.lecat@outscale.com>
2019-07-24 09:40:04 +0000

    Update chroot_centos7.sh

--- scripts/chroot_centos7.sh
+++ scripts/chroot_centos7.sh
@@ -25,7 +25,6 @@ chroot /mnt/ yum clean all
 #### OUTSCALE PACKAGES
 chroot /mnt yum install -y https://osu.eu-west-2.outscale.com/outscale-official-packages/udev/osc-udev-rules-20160516-1.x86_64.rpm
 chroot /mnt yum install -y https://osu.eu-west-2.outscale.com/outscale-official-packages/dhclient-configuration/dhclient-configuration-1.0.0-1-Centos7.x86_64.rpm
-#chroot /mnt yum install -y https://osu.eu-west-2.outscale.com/outscale-official-packages/fni/osc-fni-1.2.1-1.x86_64.rpm
 yes | cp -i /tmp/cloud.cfg /mnt/etc/cloud/cloud.cfg
 yes | cp -i /tmp/sshd_config_centos /mnt/etc/ssh/sshd_config
 chroot /mnt yum list installed > /tmp/packages
