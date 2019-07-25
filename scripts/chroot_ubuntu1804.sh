5152575782efbf3dab3f6d48de1c706c47d91ce4
Romain <romain.lecat1@gmail.com>
2019-07-25 11:13:43 +0200

    Much more reliable SCSI scan

--- scripts/chroot_ubuntu1804.sh
+++ scripts/chroot_ubuntu1804.sh
@@ -2,14 +2,14 @@
 set -e
 
 #### BASIC IMAGE
-yum install -y wget qemu-img
+yum install -y wget qemu-img sg3_utils
 cd /tmp
 wget -q http://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
 wget -q https://cloud-images.ubuntu.com/bionic/current/MD5SUMS
 if [[ $(md5sum -c MD5SUMS 2>&1 | grep -c OK) < 1 ]]; then exit 1; fi
 mv *.img bionic.img
 qemu-img convert ./bionic.img -O raw /dev/sda
-echo 1 > /sys/class/scsi_device/2\:0\:0\:0/device/rescan
+rescan-scsi-bus.sh -a
 mount /dev/sda1 /mnt
 
 #### CHROOT FIXES
