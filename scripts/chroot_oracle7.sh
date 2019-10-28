b236e4d4b22795f961e318255e853c2b73ee2244
elie.bougeard <elie.bougeard@MacBook-Pro-de-eliebougeard.local>
2019-10-28 15:59:38 +0100

    Update Oracle 7 qcow URL

--- scripts/chroot_oracle7.sh
+++ scripts/chroot_oracle7.sh
@@ -4,7 +4,7 @@ set -e
 #### BASIC IMAGE
 yum install -y wget qemu-img
 cd /tmp
-wget -q "http://omi-packer.osu.eu-west-2.outscale.com/oracle/OracleLinux-7.4-x86_64.qcow2?AWSAccessKeyId=MTXAS23YQ67OUWJWJR3C&Expires=1569495828&Signature=pkqLTZ9oWBrZYIHEyMKuXeguyyk%3D"
+wget -q "https://osu.eu-west-2.outscale.com/omi-packer-official/OracleLinux-7.6-x86_64.qcow2"
 mv Oracle* oracle.qcow2
 qemu-img convert ./oracle.qcow2 -O raw /dev/sda
 partprobe /dev/sda
