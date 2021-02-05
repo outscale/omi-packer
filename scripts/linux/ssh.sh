6bdcb3ecb4edd7540a03b8b44971c3ec1b3bc938
romain.lecat <romain.lecat@outscale.com>
2021-02-05 10:48:32 +0000

    Update ssh.sh

--- scripts/linux/ssh.sh
+++ scripts/linux/ssh.sh
@@ -6,4 +6,8 @@ yum install -y augeas
 augtool -r /mnt -s <<EOF
 set /files/etc/ssh/sshd_config/X11Forwarding no
 set /files/etc/ssh/sshd_config/PermitTunnel no
+set /files/etc/ssh/sshd_config/PermitRootLogin no
+set /files/etc/ssh/sshd_config/RSAAuthentication yes
+set /files/etc/ssh/sshd_config/PubkeyAuthentication yes
+set /files/etc/ssh/sshd_config/PasswordAuthentication no
 EOF
