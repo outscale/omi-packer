467c02bcd58624b27cd78ead37aca9dcc6bad6c3
Elston Lewis <elston.lewis@outscale.com>
2021-02-10 10:32:00 +0100

    Updated set ssh parameters for SEC compliance

--- scripts/linux/ssh.sh
+++ scripts/linux/ssh.sh
@@ -10,4 +10,7 @@ set /files/etc/ssh/sshd_config/PermitRootLogin no
 set /files/etc/ssh/sshd_config/RSAAuthentication yes
 set /files/etc/ssh/sshd_config/PubkeyAuthentication yes
 set /files/etc/ssh/sshd_config/PasswordAuthentication no
+set /files/etc/ssh/sshd_config/UseDNS no
+set /files/etc/ssh/sshd_config/ChallengeResponseAuthentication no
+set /files/etc/ssh/sshd_config/GSSAPIAuthentication no
 EOF
