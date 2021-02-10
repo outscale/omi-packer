1973c08e94f259a78071e63693cd07eab0e0d260
Romain Lecat <romain.lecat@outscale.com>
2021-02-10 18:14:32 +0100

    Fix Ubuntu 20.04 networking

--- scripts/linux/boot.sh
+++ scripts/linux/boot.sh
@@ -3,6 +3,6 @@ set -e
 
 if [[ "$1" == "ubuntu20"* ]]; then
     # Fix for network interfaces not picked up
-    sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="acpi_irq_isa=10"/g' /mnt/etc/default/grub
+    sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="acpi_irq_isa=10 net.ifnames=0"/g' /mnt/etc/default/grub
     chroot /mnt/ update-grub
 fi
