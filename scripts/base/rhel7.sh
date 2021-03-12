3b9779b774e013a84b9a13fde7fa52eef5d4d279
Romain Lecat <romain.lecat@outscale.com>
2021-03-12 15:15:00 +0100

    Update RHEL 7 version

--- scripts/base/rhel7.sh
+++ scripts/base/rhel7.sh
@@ -4,6 +4,6 @@ set -e
 #### BASIC IMAGE
 yum install -y wget qemu-img libgcrypt
 cd /tmp
-wget -q https://osu.eu-west-2.outscale.com/omi-packer-official/rhel-server-7.7-update-1-x86_64-kvm.qcow2
+wget -q https://oos.eu-west-2.outscale.com/omi/qcow/rhel-server-7.9-update-3-x86_64-kvm.qcow2
 mv *.qcow2 rhel7.qcow2
 qemu-img convert ./rhel7.qcow2 -O raw /dev/sda
