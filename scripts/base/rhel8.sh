84231c5b7b4214ccae5f7064cfb98db949776df0
Romain Lecat <romain.lecat@outscale.com>
2021-03-18 21:12:51 +0100

    Fix RHEL 8 BYOL script

--- scripts/base/rhel8.sh
+++ scripts/base/rhel8.sh
@@ -6,4 +6,4 @@ yum install -y wget qemu-img libgcrypt
 cd /tmp
 wget -q https://oos.eu-west-2.outscale.com/omi/qcow/rhel-8.3-update-2-x86_64-kvm.qcow2
 mv *.qcow2 rhel8.qcow2
-qemu-img convert ./rhel7.qcow2 -O raw /dev/sda
+qemu-img convert ./rhel8.qcow2 -O raw /dev/sda
