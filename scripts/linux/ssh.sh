dbb1780c565646bf5591809caa1c120c7b359fde
Romain Lecat <romain.lecat@outscale.com>
2021-04-20 15:16:30 +0200

    OMI-81

--- scripts/linux/ssh.sh
+++ scripts/linux/ssh.sh
@@ -3,6 +3,8 @@ set -e
 
 yum install -y augeas
 
+echo "The default user for Outscale VMs is 'outscale'." > /mnt/etc/banner
+
 augtool -r /mnt -s <<EOF
 set /files/etc/ssh/sshd_config/X11Forwarding no
 set /files/etc/ssh/sshd_config/PermitTunnel no
@@ -13,4 +15,6 @@ set /files/etc/ssh/sshd_config/PasswordAuthentication no
 set /files/etc/ssh/sshd_config/UseDNS no
 set /files/etc/ssh/sshd_config/ChallengeResponseAuthentication no
 set /files/etc/ssh/sshd_config/GSSAPIAuthentication no
+set /files/etc/ssh/sshd_config/Match[1]/Condition/User "root,centos,ubuntu,debian,ec2-user"
+set /files/etc/ssh/sshd_config/Match[1]/Settings/Banner "/etc/banner"
 EOF
