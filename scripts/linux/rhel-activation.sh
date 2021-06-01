c427f57d98208c0bff41992a8e3a6ea9e5de2c6f
Romain Lecat <romain.lecat@outscale.com>
2021-06-01 14:51:24 +0200

    Working RHEL activation without Outscale DNS + Update to RHEL 8.4

--- scripts/linux/rhel-activation.sh
+++ scripts/linux/rhel-activation.sh
@@ -2,9 +2,10 @@
 set -e
 
 if [[ "$1" == "rhel"*"csp" ]]; then
+    echo "169.254.169.254 rhsatellite.outscale.internal" >> /mnt/etc/hosts
     curl -k --output /mnt/tmp/katello-ca-consumer-latest.noarch.rpm https://169.254.169.254:4888/pub/katello-ca-consumer-latest.noarch.rpm
     chroot /mnt yum localinstall -y /tmp/katello-ca-consumer-latest.noarch.rpm
-    
+
     if [[ "$1" == "rhel8csp" ]]; then
       cp /tmp/cloudinit-specific/05_rhel8.cfg /mnt/etc/cloud/cloud.cfg.d/
     elif [[ "$1" == "rhel7csp" ]]; then
