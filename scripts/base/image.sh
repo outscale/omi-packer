0b66e682616e5d53be92ec676feb09c1a2c04edf
Romain Lecat <romain.lecat@outscale.com>
2021-11-08 14:28:00 +0100

    Supports wider range of images

--- scripts/base/image.sh
+++ scripts/base/image.sh
@@ -2,8 +2,30 @@
 set -e
 
 #### BASIC IMAGE
-yum install -y wget qemu-img libgcrypt
+yum install -y epel-release
+yum install -y wget qemu-img libgcrypt p7zip p7zip-plugins
 cd /tmp
 wget -q "$1"
-mv *.qcow2 image.qcow2
-qemu-img convert ./image.qcow2 -O raw /dev/sda
+
+# Extract image if gunzipped
+if [ -f *.gz ]; then 
+    7z x *.gz
+fi
+
+# Fix unclear file extensions
+if [ -f *.img ]; then 
+    if [[ `file *.img` =~ "QCOW" ]]; then 
+        mv *.img image.qcow2
+    else
+        exit 1
+    fi
+fi
+
+# Copy image to disk
+if [ -f *.qcow2 ]; then 
+    qemu-img info *.qcow2
+    qemu-img convert *.qcow2 -O raw /dev/sda
+fi
+if [ -f *.raw ]; then 
+    dd if=*.raw of=/dev/sda bs=1G status=progress conv=sparse
+fi
