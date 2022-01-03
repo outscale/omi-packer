b11e3b2e12237328b7cb58b7bc0344bdd877d38d
Romain Lecat <romain.lecat@outscale.com>
2022-01-03 13:44:41 +0100

    Fix broken ArchLinux build

--- scripts/base/arch.sh
+++ scripts/base/arch.sh
@@ -12,6 +12,6 @@ qemu-img convert ./arch.qcow2 -O raw /dev/sda
 #### CUSTOM KERNEL WITH BTRFS SUPPORT
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 yum install -y https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm
-yum --disablerepo=\* --enablerepo=elrepo-kernel --enablerepo=elrepo-testing -y install kernel-ml btrfs-progs
+yum --disablerepo=\* --enablerepo=elrepo-kernel --enablerepo=elrepo-testing -y install kernel-ml
 grub2-set-default 0
 reboot
