9f2c61bac9f4e4ab7b12d117a3748a1c0e41f75c
Romain Lecat <romain.lecat@outscale.com>
2022-02-09 16:09:09 +0100

    Upgrade RockyLinux to 8.5

--- scripts/base/rocky8.sh
+++ scripts/base/rocky8.sh
@@ -4,6 +4,6 @@ set -e
 #### BASIC IMAGE
 yum install -y wget qemu-img libgcrypt
 cd /tmp
-wget -q https://dl.rockylinux.org/pub/rocky/8.4/images/Rocky-8-GenericCloud-8.4-20210620.0.x86_64.qcow2
+wget -q https://dl.rockylinux.org/pub/rocky/8.5/images/Rocky-8-GenericCloud-8.5-20211114.2.x86_64.qcow2
 mv *.qcow2 centos8.qcow2
 qemu-img convert ./centos8.qcow2 -O raw /dev/sda
