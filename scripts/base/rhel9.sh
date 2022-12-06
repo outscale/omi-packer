907861c66efadc74028ad46bb68155e05916b57c
Romain Lecat <romain.lecat@outscale.com>
2022-12-06 17:54:40 +0100

    Revert "Bumped RHEL-9 elrepo release to 9 (#16)"
    This reverts commit 00f66e98d466747e8318b667b5ae630fa13aff6a.

--- scripts/base/rhel9.sh
+++ scripts/base/rhel9.sh
@@ -9,7 +9,7 @@ qemu-img convert ./*.qcow2 -O raw /dev/sda
 
 #### CUSTOM KERNEL WITH RECENT XFS SUPPORT
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-yum install -y https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm
+yum install -y https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm
 yum --disablerepo=\* --enablerepo=elrepo-kernel --enablerepo=elrepo-testing -y install kernel-ml
 grub2-set-default 0
 reboot
