bc44021a8c2019d12af6c17a9ebb694c5f8e9d0f
Elston Lewis <elston.lewis@outscale.com>
2023-01-31 17:24:06 +0100

    Added cloudinit Hotplug FNI support for Ubuntu 22.04 and Rockylinux 9

--- scripts/linux/cloud-init.sh
+++ scripts/linux/cloud-init.sh
@@ -5,5 +5,5 @@ cp /tmp/cloudinit/*.cfg /mnt/etc/cloud/cloud.cfg.d/
 
 if [[ "$1" == "ubuntu2204" ]] || [[ "$1" == "rocky9" ]]; then
 	#Add FNI Hotplug support
-	cp /tmp/cloudinit-specific/cloud.cfg /mnt/etc/cloud/cloud.cfg
+	cp /tmp/cloudinit-specific/06_hotplug.cfg /mnt/etc/cloud/cloud.cfg.d/
 fi
