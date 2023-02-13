2e20f34fb1bb6d0c5b867b226701977173fa51a5
Elston Lewis <elston.lewis@outscale.com>
2023-02-13 11:13:20 +0100

    Cleaned up start.ps1 and removed mtu-9000.ps1

--- files/windows/scripts/start.ps1
+++ files/windows/scripts/start.ps1
@@ -296,20 +296,6 @@ try {
   Disable-NetAdapterBinding -InterfaceAlias "Ethernet" -ComponentID ms_tcpip6
   Disable-NetAdapterBinding -InterfaceAlias Ethernet -ComponentID ms_tcpip6
 
-  $netAdapter=Get-NetAdapterAdvancedProperty -Name "Ethernet"
-
-  # Get Windows version
-  $os_version = (Get-WmiObject -class Win32_OperatingSystem).Caption
-  if ($os_version.contains("2019")) {
-    $mtu = $netAdapter | Where-Object {$_.DisplayName -eq "Init.MTUSize"}
-    if ($mtu.DisplayValue -ne 8950) {
-      Set-NetAdapterAdvancedProperty -Name "Ethernet" -DisplayName "Init.MTUSize" -DisplayValue 8950
-      WriteLog("Set-NetAdapterAdvancedProperty Ethernet Init.MTUSize 8950")
-    } else {
-      WriteLog("NetAdapterAdvancedProperty Ethernet Init.MTUSize 8950 already set")
-    }
-  }
-
   $mtu = (Get-NetIPInterface -InterfaceAlias "Ethernet").NlMtu
   if ($mtu -ne 8950) {
     Set-NetAdapterAdvancedProperty -Name "Ethernet" -DisplayName "Jumbo Packet" -DisplayValue "9014"
