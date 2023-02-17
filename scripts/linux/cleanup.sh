c9646697e1f79fede8932fd55d7510f04d7859f6
Elston Lewis <118978679+ElstonLewis@users.noreply.github.com>
2023-02-17 16:14:15 +0100

    fixed dhcp folder deletion and type-o (#36)
    * Updated windows scripts
    
    * Added sql hostname update to windows first boot script
    
    * Updated Windows first boot script
    
    * Fixed deletion of dhcp folder and type-o

--- scripts/linux/cleanup.sh
+++ scripts/linux/cleanup.sh
@@ -9,7 +9,7 @@ rm -rf /mnt/var/cache/yum
 rm -rf /mnt/root/.ssh
 rm -rf /mnt/root/.bash_history
 rm -rf /mnt/tmp/*
-rm -rf /mnt/var/lib/dhcp/
+rm -rf /mnt/var/lib/dhcp/*
 rm -rf /mnt/var/tmp/*
 find /mnt/var/log ! -type d -exec rm '{}' \;
 rm -rf /mnt/var/lib/cloud/*
