8892e6ef2d77347268f6fe339a8c0fa40053bfda
Rémi Jouannet <remi.jouannet@outscale.com>
2023-06-06 12:24:03 +0200

    enable fstrim.timer for every linux

--- scripts/linux/fstrim.sh
+++ scripts/linux/fstrim.sh
@@ -4,5 +4,5 @@ set -e
 #enable by default on debian/ubuntu/opensuse
 if [[ "$1" == "centos"* ]] || [[ "$1" == "rocky"* ]] || [[ "$1" == "alma"* ]] || [[ "$1" == "rhel"* ]] || [[ "$1" == "arch"* ]]; then
     #Execute fstrim weekly
-    systemctl enable fstrim.timer
+    chroot /mnt/ systemctl enable fstrim.timer
 fi
