a57eef199f5c6dcee1b07ffd5e9b0f66b4025986
elston.lewis <elston.lewis@outscale.com>
2023-11-22 13:05:39 +0100

    Added echo for variables set for packer

--- build.sh
+++ build.sh
@@ -28,11 +28,15 @@ if [ "$VOL_SIZE" == 0 ] || [ -z "$VOL_SIZE" ]; then
 else
     export PKR_VAR_volsize=$VOL_SIZE
 fi
+echo "packer env checks..."
 echo "product_code: `echo $OUTSCALE_PRODUCT_CODES`"
 echo "packer version `/bin/packer --version`"
 echo "packer plugins: `/bin/packer plugins installed`"
 
-/bin/packer init -upgrade ./config.pkr.hcl
+echo "executing /bin/packer init -upgrade ."
+/bin/packer init -upgrade .
+
+echo "executing /bin/packer build ./$PACKER_SCRIPT | tee /usr/local/packer/logs/$UOMI_NAME.log"
 /bin/packer build ./$PACKER_SCRIPT | tee /usr/local/packer/logs/$UOMI_NAME.log
 
 # Workaround for bad Packer exit code
