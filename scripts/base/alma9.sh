a6bf99e5289d0edbded4a89b23f5670f4929e885
elston.lewis <elston.lewis@outscale.com>
2024-06-14 13:33:32 +0200

    Updated image url for Alma9

--- scripts/base/alma9.sh
+++ scripts/base/alma9.sh
@@ -4,7 +4,7 @@ set -e
 #### BASIC IMAGE
 yum install -y wget qemu-img libgcrypt
 cd /tmp
-wget -q https://repo.almalinux.org/almalinux/9.3/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2
+wget -q https://repo.almalinux.org/almalinux/9.4/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2
 qemu-img convert ./*.qcow2 -O raw /dev/sda
 
 #### CUSTOM KERNEL WITH RECENT XFS SUPPORT
