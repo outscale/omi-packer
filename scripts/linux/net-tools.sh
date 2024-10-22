1adba8a69678eae1340db1913a674b3d1f2bcbba
Elston LEWIS <elston.lewis@outscale.com>
2024-10-22 14:15:08 +0200

    Updated builder scripts for Debian 12 dhcp ntp issue

--- scripts/linux/net-tools.sh
+++ scripts/linux/net-tools.sh
@@ -4,3 +4,8 @@ set -e
 if [[ "$1" == "debian12" ]] || [[ "$1" == "ubuntu24"* ]]; then
     chroot /mnt/ apt install net-tools -y
 fi
+
+# Netplan file permission fix OMI-239
+if [[ "$1" == "debian12" ]]; then
+    chmod 644 /mnt/etc/netplan/50-cloud-init.yaml
+fi
--- scripts/linux/ntp.sh
+++ scripts/linux/ntp.sh
@@ -5,3 +5,8 @@ if [[ "$1" == "debian12" ]]; then
     chroot /mnt/ apt update -y
     chroot /mnt/ apt install chrony -y
 fi
+
+# make executable chronyd script fix OMI-239
+if [[ "$1" == "debian12" ]]; then
+    chmod +x /mnt/etc/dhcp/dhclient-exit-hooks.d/chrony
+fi
