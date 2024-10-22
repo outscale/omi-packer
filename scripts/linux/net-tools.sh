25844a7a8e874510fb19f138a1f7cccfbab8276c
Elston LEWIS <elston.lewis@outscale.com>
2024-10-22 14:48:16 +0200

    Disabled netplan file update

--- scripts/linux/net-tools.sh
+++ scripts/linux/net-tools.sh
@@ -6,6 +6,6 @@ if [[ "$1" == "debian12" ]] || [[ "$1" == "ubuntu24"* ]]; then
 fi
 
 # Netplan file permission fix OMI-239
-if [[ "$1" == "debian12" ]]; then
-    chmod 644 /mnt/etc/netplan/50-cloud-init.yaml
-fi
+#if [[ "$1" == "debian12" ]]; then
+#    chmod 644 /mnt/etc/netplan/50-cloud-init.yaml
+#fi
